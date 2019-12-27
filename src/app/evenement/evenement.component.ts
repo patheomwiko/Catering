f68585b3983ded913e5e9124dfb794fbde759bbc
patheomwiko <patheomwiko45@gmail.com>
2019-12-27 02:38:06 +0200

    modification sur le composant evenement

--- src/app/evenement/evenement.component.ts
+++ src/app/evenement/evenement.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AnniversaireComponent} from './anniversaire/anniversaire.component';
+import {CongreComponent} from './congre/congre.component';
+import {CocktailComponent} from './cocktail/cocktail.component';
+import {FuneraillesComponent} from './funerailles/funerailles.component';
+import {MariageComponent} from './mariage/mariage.component';
+import {Route, RouterModule, Routes} from '@angular/router';
+const routes: Routes = [
+  {path: 'anniversaire', component: AnniversaireComponent},
+  {path: 'congre', component: CongreComponent},
+  {path: 'cocktail', component: CocktailComponent},
+  {path: 'funerailles', component: FuneraillesComponent},
+  {path: 'mariage', component: MariageComponent},
+  {path: '', redirectTo: '/anniversaire', pathMatch: 'full'}
+]
 @Component({
   selector: 'app-evenement',
   templateUrl: './evenement.component.html',
