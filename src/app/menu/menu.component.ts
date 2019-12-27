8018326112e3f8de0a473432640efe2094399d5c
patheomwiko <patheomwiko45@gmail.com>
2019-12-28 01:21:35 +0200

    modification du composant menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Route, RouterModule, Routes} from '@angular/router';
+import {ChaudComponent} from './chaud/chaud.component';
+import {DejeunerComponent} from './dejeuner/dejeuner.component';
+import {FroidComponent} from './froid/froid.component';
+import {OeuvreComponent} from './oeuvre/oeuvre.component';
+import {PatisserieComponent} from './patisserie/patisserie.component';
+const routes: Routes = [
+  {path: 'chaud', component: ChaudComponent},
+  {path: 'dejeuner', component: DejeunerComponent},
+  {path: 'froid', component: FroidComponent},
+  {path: 'oeuvre', component: OeuvreComponent},
+  {path: 'patesserie', component: PatisserieComponent},
+  {path: '', redirectTo: '/chaud', pathMatch: 'full'}
+]
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
